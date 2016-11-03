cd54227d9ac2d8b6daee706a8ffdc5aae07365f5
Tracy Yang <jewel7meme@gmail.com>
2016-11-03 11:40:16 -0500

    Request  407: Read URL File instead of Stream

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -11,8 +11,8 @@ namespace SingleResponsibilityPrinciple
     {
         static void Main(string[] args)
         {
-            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades4.txt");
-
+        //var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades4.txt");
+         var tradeStream = "http://faculty.css.edu/tgibbons/trades4.txt";
             var tradeProcessor = new TradeProcessor();
             tradeProcessor.ProcessTrades(tradeStream);
 
--- SingleResponsibilityPrinciple/TradeProcessor.cs
+++ SingleResponsibilityPrinciple/TradeProcessor.cs
@@ -23,7 +23,19 @@ namespace SingleResponsibilityPrinciple
             }
             return tradeData;
         }
-
+        private IEnumerable<string> ReadURLTradeData(string url)
+        {
+            var tradeData = new List<string>();
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
+            using (var reader = new StreamReader(stream)) {
+                string line;
+                while ((line = reader.ReadLine()) != null) {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+        }
         private IEnumerable<TradeRecord> ParseTrades(IEnumerable<string> tradeData)
         {
             var trades = new List<TradeRecord>();
@@ -144,10 +156,11 @@ namespace SingleResponsibilityPrinciple
 
             LogMessage(type, "{0} trades processed", trades.Count());
         }
+  
 
-        public void ProcessTrades(Stream stream)
+        public void ProcessTrades(String url)
         {
-            var lines = ReadTradeData(stream);
+            var lines = ReadURLTradeData(url);
             var trades = ParseTrades(lines);
             StoreTrades(trades);
         }
